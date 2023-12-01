1f54f4eb9481bb1db4e1c5d1a761a26305e35036
Nicolas Firpo <nicolas.firpo@davinci.edu.ar>
2023-12-01 20:19:37 -0300

    feat: fix backend.php & perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -1,5 +1,28 @@
 
+
 //funcion para redireccionar a la pagina de historial
 function redirectToMain() {
     window.location.href = '../index.html';
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    const userEmail = "demo@davinci.com.ar";
+
+    fetch(`../php/backend.php?email=${userEmail}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                const usuario = data[0];
+                document.getElementById('nombreApellido').innerText = `${usuario.nombre} ${usuario.apellido}`;
+                document.getElementById('materia').innerText = usuario.materia;
+                document.getElementById('comision').innerText = usuario.comision;
+
+                // Obtener la imagen y establecer el atributo src
+                const imagenPerfil = document.getElementById('imagenPerfil');
+                imagenPerfil.src = `../img/${usuario.imagen}`;
+            } else {
+                console.error('Error: No se encontraron datos para el usuario.');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
--- php/backend.php
+++ php/backend.php
@@ -1,50 +1,12 @@
 <?php
-
-// Función para obtener la lista de datos de un usuario
 function obtenerDatosUsuario($email) {
-    
     if ($email === "demo@davinci.com.ar") {
         return array(
-            array('nombre' => 'Nicolas', 'apellido' => 'Firpo', 'materia' => 'PWA', 'comision' => 'DWM3AV')
+            array('nombre' => 'Nicolas', 'apellido' => 'Firpo', 'materia' => 'PWA', 'comision' => 'DWM3AV', 'imagen' => 'perfil.jpg')
         );
     } else {
         return array(); 
     }
-}
-
-function agregarDatos($email, $nombre, $apellido, $materia, $comision) {
-   
-    if ($email === "demo@davinci.com.ar") {
-        $datosUsuario = obtenerDatosUsuario($email);
-
-        $nuevoDato = array('nombre' => $nombre, 'apellido' => $apellido, 'materia' => $materia, 'comision' => $comision);
-        $datosUsuario[] = $nuevoDato;
-
-        // En este punto, deberías guardar $datosUsuario en tu sistema de almacenamiento
-        // Puede ser una base de datos, un archivo, etc.
-
-        return true; // Devolvemos true si la operación fue exitosa
-    } else {
-        return false; // Devolvemos false si el usuario no está registrado
-    }
-}
-
-// Manejar la solicitud POST desde la página principal
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtener los datos de la solicitud POST
-    $email = $_POST["email"];
-    $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $materia = $_POST["materia"];
-    $comision = $_POST["comision"];
-
-    // Agregar el nuevo dato al usuario
-    $exito = agregarDatos($email, $nombre, $apellido, $materia, $comision);
-
-    // Devolver una respuesta JSON indicando el resultado
-    header("Content-Type: application/json");
-    echo json_encode(array('exito' => $exito));
-    exit();
 }
 
 // Manejar la solicitud GET desde la página principal
@@ -60,5 +22,4 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     echo json_encode($datosUsuario);
     exit();
 }
-
 ?>
